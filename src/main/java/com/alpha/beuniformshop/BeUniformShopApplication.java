ad895eed0b1f89205e15e835d3e796f0a95788c7
Kaisamira <thienthan726@gmail.com>
2023-11-23 00:52:02 +0700

    first commit

---
+++ src/main/java/com/alpha/beuniformshop/BeUniformShopApplication.java
@@ -0,0 +1,13 @@
+package com.alpha.beuniformshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeUniformShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeUniformShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alpha/beuniformshop/BeUniformShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.beuniformshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeUniformShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
